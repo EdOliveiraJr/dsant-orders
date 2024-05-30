3216459c43c5c980cba138717371c2dba032bc48
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2024-05-30 17:04:14 -0300

    feat: includes primeflex lib configuration

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
-import './assets/main.scss'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import './assets/main.scss';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+import 'primevue/resources/themes/aura-light-green/theme.css';
+import 'primevue/resources/primevue.min.css';
+import primevue from 'primevue/config';
 
-const app = createApp(App)
 
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(router);
+app.use(primevue);
+
+app.mount('#app');
